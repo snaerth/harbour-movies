e6cc18cc22f1a822d1613956d8f62bbf28a69cba
Snær Seljan Þóroddsson <snaerth@gmail.com>
2023-02-12 16:22:17 +0000

    add apollo explorer to graphql api

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from '../../lib/graphql/typeDefs';
@@ -8,6 +9,12 @@ export const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const server = new ApolloServer({
   schema,
+  plugins: [
+    ApolloServerPluginLandingPageProductionDefault({
+      embed: true,
+      graphRef: 'my-graph@current',
+    }),
+  ],
 });
 
 export default startServerAndCreateNextHandler(server);
